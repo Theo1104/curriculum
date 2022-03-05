32a34c8f78c7b17ef4166a9385867012261571e6
Heli Rosales <helirosalesvillanueva@gmail.com>
2022-03-05 00:01:19 -0300

    AAñadido texto arriba de la imagen

--- components/Aptitude.js
+++ components/Aptitude.js
@@ -1,10 +1,12 @@
 import Image from "next/image"
 import Box from "@mui/material/Box"
+import Typography from '@mui/material/Typography';
 
 const Aptitude = () => {
   return (
     <div>
         <Box>
+            <Typography variant="h4" component="h1" sx={{marginLeft:10, marginTop:5,position:"absolute", zIndex:2, color:"white"}}>Aptitudes</Typography>
             <Image src="/Code2.jpg" width="100%" height={60} layout="responsive"/>
         </Box>
     </div>
