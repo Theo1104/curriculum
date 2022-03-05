0e84360976957750a2491534b6b79910aa7b9b8e
Heli Rosales <helirosalesvillanueva@gmail.com>
2022-03-04 23:46:56 -0300

    Agregada imagen de las aptitudes

---
+++ components/Aptitude.js
@@ -0,0 +1,14 @@
+import Image from "next/image"
+import Box from "@mui/material/Box"
+
+const Aptitude = () => {
+  return (
+    <div>
+        <Box>
+            <Image src="/Code2.jpg" width="100%" height={60} layout="responsive"/>
+        </Box>
+    </div>
+  )
+}
+
+export default Aptitude
