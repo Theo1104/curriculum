d805efe0ec1b508c053f31b41854d4dca0729854
Heli Rosales <helirosalesvillanueva@gmail.com>
2022-03-11 16:10:58 -0300

    Se añadio el componente Form al proyecto

---
+++ components/Form.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from "../styles/Header.module.css"
+import Box from "@mui/material/Box"
+
+const Form = () => {
+  return (
+    <div className={styles}>
+        <Box>
+
+        </Box>
+        <Box>
+
+        </Box>
+    </div>
+  )
+}
+
+export default Form
